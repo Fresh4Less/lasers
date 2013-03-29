070e6b91e39d15245b9d2881de47f03382e946bf
Micah Johnston <Micah@PAULs-Mac-mini.local>
2013-03-29 13:01:34 -0600

    blocks are now defined by vectors

--- block.lua
+++ block.lua
@@ -2,8 +2,10 @@ Class = require "hump.class"
 vector = require "hump.vector"
 
 Block = Class {
-	init = function(self, pos) 
+	init = function(self, pos, size) 
 		self.pos = pos
+        self.shape = { vector(size.x, 0), vector(0, size.y), vector(-size.x, 0), vector(0, -size.y) }
+        self.size = size
 	end
 }
 function Block:update(dt)
@@ -11,7 +13,16 @@ function Block:update(dt)
 end
 function Block:draw()
 	love.graphics.setColor(0, 0, 255)
-	love.graphics.rectangle("fill", self.pos.x, self.pos.y, 32, 32)
+	
+    local pos = self.pos
+    local verts = {}
+    for i, v in ipairs(self.shape) do
+        table.insert(verts, pos.x)
+        table.insert(verts, pos.y)
+        pos = pos + v
+    end
+
+    love.graphics.polygon("line", verts)
 end
 
 return Block
