70b8919fbc452e9547d03069b6636446b39d4687
Elliot Hatch <elliot.hatch@gmail.com>
2013-03-28 23:44:10 -0600

    create player class

--- main.lua
+++ main.lua
@@ -1,5 +1,31 @@
-State = require "hump.gamestate"
+Gamestate = require "hump.gamestate"
+Camera = require "hump.camera"
 
-function love.update()
+local game = {}
 
+function game:enter()
+	self.cam = Camera(0, 0)
 end
+function game:update()
+	
+end
+function game:draw()
+	
+end
+
+function love.load()
+	Gamestate.registerEvents()
+	Gamestate.switch(game)
+end
+
+Player = Class {
+	init = function(self, pos) {
+		self.pos = pos
+	}
+}
+function Player:update(dt) {
+
+}
+function Player:draw() {
+
+}
