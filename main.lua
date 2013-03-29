5895371c53be65a4240959dd62cb695310f0de4a
Micah Johnston <Micah@PAULs-Mac-mini.local>
2013-03-29 13:01:59 -0600

    accomodate multiple walls

--- main.lua
+++ main.lua
@@ -11,16 +11,25 @@ local game = {}
 function game:enter()
 	self.cam = Camera(0, 0)
 	
-	self.player = Player(vector(100, 100))
-	self.block = Block(vector(200, 200))
+	self.walls = {}
+
+	self.player = Player(vector(100, 100), self.walls)
+
+	table.insert(self.walls, Block(vector(200, 200), vector(32, 32)))
+	table.insert(self.walls, Block(vector(0, 0), vector(800, 600)))
 end
 function game:update(dt)
 	self.player:update(dt)
-	self.block:update(dt)
+
+	for i,block in ipairs(self.walls) do
+		block:update(dt)
+	end
 end
 function game:draw()
 	self.player:draw()
-	self.block:draw()
+	for i,block in ipairs(self.walls) do
+		block:draw(dt)
+	end
 end
 
 function love.load()
